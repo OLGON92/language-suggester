3ad7a015ada9d24b89fefc8ccc10c19119bb6a23
OLGON92 <ogon318@gmail.com>
2022-04-09 02:05:08 -0700

    add branching.

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
     const userMathOrReader=$("input:radio[name=math-or-reader]:checked").val();
     const userPulse=$("input:radio[name=pulse]:checked").val();
     const userCardioOrLifting=$("input:radio[name=cardio-or-lift]:checked").val();
-    
+
+    let result;
+    if (userFavoriteAnimal === "Snake" || userFavoriteAnimal === "Bear") {
+      result = "Go";
+    } else if (userMorningOrNight === "Night") {
+      result = "C#";
+    } else if (userMathOrReader === "Math") {
+      result = "JavaScript";
+    } else if (userPulse === "Yes") {
+      result = "Rust";
+    } else if (userCardioOrLifting === "Lift") {
+      result = "Python";
+    }
+
+    $("results").text(result);
+    $("results-now").show();
+
+  });
+});
